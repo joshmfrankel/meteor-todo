1a5b005b4e364f673ada84664b154eb8f5c6a00c
Josh Frankel <joshmfrankel@gmail.com>
2015-09-15 19:36:01 -0400

    Add basic add tasks logic

--- todos.js
+++ todos.js
@@ -14,6 +14,21 @@ if(Meteor.isClient){
       return Todos.find({}, {sort: {createdAt: -1}});
     }
   });
+
+  // Events block
+  // addTodo is the template name
+  Template.addTodo.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var todoName = $('[name="todoName"]').val();
+      Todos.insert({
+        name: todoName,
+        completed: false,
+        createdAt: new Date()
+      });
+      $('[name="todoName"]').val('');
+    }
+  });
 }
 
 if(Meteor.isServer){
