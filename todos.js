742fac74b4d76364a402efca27a63b9a9f400a6a
Josh Frankel <joshmfrankel@gmail.com>
2015-09-15 19:55:11 -0400

    Add update todo, Add css

--- todos.js
+++ todos.js
@@ -39,6 +39,11 @@ if(Meteor.isClient){
       if (confirm) {
         Todos.remove({ _id: documentId });
       }
+    },
+    'keyup [name=todoItem]': function(event) {
+      var documentId = this._id;
+      var todoItem = $(event.target).val();
+      Todos.update({ _id: documentId }, {$set: { name: todoItem }});
     }
   });
 }
