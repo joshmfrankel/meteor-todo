efc16cbe3f424f072081f10542e13284e494e620
Josh Frankel <joshmfrankel@gmail.com>
2015-09-17 10:42:15 -0400

    Todo count - Finish Managing todos section 3

--- todos.js
+++ todos.js
@@ -22,6 +22,15 @@ if(Meteor.isClient){
     }
   });
 
+  Template.todosCount.helpers({
+    'totalTodos': function() {
+      return Todos.find().count();
+    },
+    'completedTodos': function() {
+      return Todos.find({ completed: true }).count();
+    }
+  });
+
 
   /**
    * Events
